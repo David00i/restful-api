e53acb1f2188a2f55d66419f607dbec3a5be8a4a
David 00i <ooidavid10@gmail.com>
2022-05-16 14:30:39 +0800

    update user.js

--- user.js
+++ user.js
@@ -15,19 +15,39 @@ class User {
 	 */
 	static async register(username, password, phone) {
 		// TODO: Check if username exists
+	    let user = await users.findOne({ "username": username });
+		if (user) {
+			return 'duplicate user name';
+		} else {
 		// TODO: Hash password
+        const hashpassword = await bcrypt.hash(password, 10);
 		// TODO: Save user to database
+        await users.insertOne({"username": username, "password": hashpassword});
+		return "user created";
+		}
 		// faker.js
 		// return
 	}
 
 	static async login(username, password) {
-		// TODO: Check if username exists
-		findOne({username: username})
-
-		// TODO: Validate password
-		compared()
-		return false
+		return users.findOne({
+			"username":username
+		}).then(async user => {
+			// TODO: Check if username exists
+			if(user) {
+				// TODO: Validate password
+				const match = await bcrypt.compare(password, user.password); 
+				if(match) {
+					return "login successful";
+				}
+				else {
+					return "invalid password";
+				}	
+			}
+			else {
+				return "invalid username";
+			}
+		})
 
 		// TODO: Return user object
 		return user;
